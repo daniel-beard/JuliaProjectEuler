1b5d7fb8dca37b63cc2d56a88a9a304ba19cb1d0
Daniel Beard <dbeard@groupon.com>
2015-01-06 16:02:09 -0800

    Solution to problem 32

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ JuliaProjectEuler
 
 Problems from ProjectEuler solved using Julia
 
-- Problem 1-31  ✓
+- Problem 1-32  ✓
---
+++ p32.jl
@@ -0,0 +1,35 @@
+#=
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+=#
+function nd(a) # Reconstruct a digit from an array
+  multiplicand = 1
+  result = 0
+  for i in reverse(1:length(a))
+    digit = a[i]
+    result += multiplicand * digit
+    multiplicand *= 10
+  end
+  result
+end
+
+function calc()
+  matches = Int64[]
+  for c in permutations([1:9])
+    c1,c2,c3 = c[1:2],c[3:5],c[6:9] # 2x3
+    d1,d2,d3 = c[1:1],c[2:5], c[6:9] # 1x4
+    if nd(c1) * nd(c2) == nd(c3)
+      push!(matches, nd(c3))
+    end
+    if nd(d1) * nd(d2) == nd(d3)
+      push!(matches, nd(d3))
+    end
+  end
+  sum(unique(matches))
+end
+@time println(calc())
