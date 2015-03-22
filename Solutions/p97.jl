cf7ee36a1070f1b2d8bf1aa4b23c931f27f72480
Daniel Beard <dbeard@groupon.com>
2015-03-22 12:09:24 -0700

    Solution to p97

---
+++ Solutions/p97.jl
@@ -0,0 +1,12 @@
+#=
+The first known prime found to exceed one million digits was discovered in 1999, and is a Mersenne prime of the form 26972593−1; it contains exactly 2,098,960 digits. Subsequently other Mersenne primes, of the form 2p−1, have been found which contain more digits.
+
+However, in 2004 there was found a massive non-Mersenne prime which contains 2,357,207 digits: 28433×2^7830457+1.
+
+Find the last ten digits of this prime number.
+=#
+function calc()
+  x = BigInt(28433) * (BigInt(2)^7830457) + 1
+  "$x"[end-9:end]
+end
+@time println(calc())
