bbd5a1cdc98b7e36c65861d6d0ce8c597d8da4f9
Daniel Beard <dbeard@groupon.com>
2015-03-22 14:23:32 -0700

    Solution to p63

---
+++ Solutions/p63.jl
@@ -0,0 +1,18 @@
+#=
+The 5-digit number, 16807=75, is also a fifth power. Similarly, the 9-digit number, 134217728=89, is a ninth power.
+
+How many n-digit positive integers exist which are also an nth power?
+=#
+function calc()
+  count = 0
+  for n in 1:100
+    for p in 1:100
+      x = BigInt(n)^p
+      if length("$x") == p
+        count = count + 1
+      end
+    end
+  end
+  count
+end
+@time println(calc())
