1f9cda7fc3021022be66866750e0726b3e7ebadd
Daniel Beard <dbeard@groupon.com>
2015-03-22 19:28:11 -0700

    Solution to p79

---
+++ Solutions/p79.jl
@@ -0,0 +1,37 @@
+#=
+A common security method used for online banking is to ask the user for three random characters from a passcode. For example, if the passcode was 531278, they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.
+
+The text file, keylog.txt, contains fifty successful login attempts.
+
+Given that the three characters are always asked for in order, analyse the file so as to determine the shortest possible secret passcode of unknown length.
+=#
+include("projecteulerutils.jl")
+function matches(list, permutation)
+  for i in list
+    d = reverse(digits(i))
+    if findfirst(permutation, d[1]) > findfirst(permutation, d[2]) || findfirst(permutation, d[2]) > findfirst(permutation, d[3])
+      return false
+    end
+  end
+  true
+end
+
+function calc()
+  strings = sort!(vec(readdlm(dirname(@__FILE__()) * "/../Resources/p79.txt", ',', String)))
+  # Create number set
+  numberset = Int64[]
+  list = Int64[]
+  for string in strings
+    [push!(numberset, x) for x in digits(int(string))]
+    push!(list, int(string))
+  end
+  numberset = unique(numberset)
+
+  # Permutations
+  for p in permutations(numberset)
+    if matches(list, p)
+      return nd(p)
+    end
+  end
+end
+@time println(calc())
