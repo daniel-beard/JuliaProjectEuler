beb17007ddea19fce3fe198172c8ab987731db6c
Daniel Beard <dbeard@groupon.com>
2015-04-12 18:45:57 -0700

    Solution to p206

---
+++ Solutions/p206.jl
@@ -0,0 +1,17 @@
+#=
+Find the unique positive integer whose square has the form 1_2_3_4_5_6_7_8_9_0,
+where each “_” is a single digit.
+=#
+function calc()
+  # This is kinda icky.
+  for i in 1238902700:3238902700
+    s = i^2
+    if length("$s") != 19
+      continue
+    end
+    if ismatch(r"1\d2\d3\d4\d5\d6\d7\d8\d9\d0", "$s")
+      return int64(sqrt(s))
+    end
+  end
+end
+@time println(calc())
